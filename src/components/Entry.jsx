a52046906a25f3e5ed728a68c2a0702d30248596
Pavel Merekin <merekin33@gmail.com>
2025-03-17 20:16:04 +0700

    Entry props simplified

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,17 +1,14 @@
 import locationMarker from '../assets/location-marker.svg'
 
-export default function Entry({
-  imgInfo,
-  country,
-  title,
-  googleMapsLink,
-  dates,
-  text,
-}) {
+export default function Entry({ countryData }) {
   return (
     <article className="entry-wrapper">
       <div className="entry-main-photo-container">
-        <img src={imgInfo.src} alt={imgInfo.alt} className="entry-main-photo" />
+        <img
+          src={countryData.img.src}
+          alt={countryData.img.alt}
+          className="entry-main-photo"
+        />
       </div>
       <div className="entry-content">
         <div className="location-wrapper">
@@ -20,17 +17,17 @@ export default function Entry({
             alt="Location marker"
             className="location-marker"
           />
-          <p className="location-country-name">{country}</p>
+          <p className="location-country-name">{countryData.country}</p>
           <a
-            href={googleMapsLink}
+            href={countryData.googleMapsLink}
             target="blank"
             className="location-maps-link">
             View on Google Maps
           </a>
         </div>
-        <h2 className="country-name">{title}</h2>
-        <p className="visit-dates">{dates}</p>
-        <p className="info-text">{text}</p>
+        <h2 className="country-name">{countryData.title}</h2>
+        <p className="visit-dates">{countryData.dates}</p>
+        <p className="info-text">{countryData.text}</p>
       </div>
     </article>
   )
