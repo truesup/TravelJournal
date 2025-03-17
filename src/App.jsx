6e23f715970807648d2eb09036a8d9883caf92b3
Pavel Merekin <merekin33@gmail.com>
2025-03-17 08:03:29 +0700

    initial settings

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Header from './components/Header'
+
+export default function App() {
+  return <Header />
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/globe.svg'
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <img src={logo} alt="Journal Logo" />
+      </nav>
+    </header>
+  )
+}
